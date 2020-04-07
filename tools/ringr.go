a4b95c381fa36f044b7a94836972084e9d309ac1
lbs <943788110@qq.com>
2020-04-07 22:22:33 +0800

    add methods to ting

--- tools/ringr.go
+++ tools/ringr.go
@@ -19,3 +19,37 @@ func (r *Ringr) init() *Ringr {
 	r.next = r
 	return r
 }
+
+// get the next value
+func (r *Ringr) Next() *Ringr {
+	if r.next == nil {
+		return r.init()
+	}
+	return r.next
+}
+
+// get the pre value
+func (r *Ringr) Prev() *Ringr {
+	if r.pre == nil {
+		return r.init()
+	}
+	return r.pre
+}
+
+// move the value by given steps
+func (r *Ringr) Move(n int) *Ringr {
+	if r.next == nil {
+		return r.init()
+	}
+	switch  {
+	case n < 0:
+		for ; n < 0; n++ {
+			r = r.pre
+		}
+	case n > 0:
+		for ; n > 0; n-- {
+			r = r.next
+		}
+	}
+	return r
+}
