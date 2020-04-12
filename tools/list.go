ae4ac17ae6c784d4a904a09038baaa21e6c941b1
lbs <943788110@qq.com>
2020-04-12 20:32:21 +0800

    add methods to list

--- tools/list.go
+++ tools/list.go
@@ -84,6 +84,29 @@ func (l *List) insert(e, at *Element) *Element {
 	at.next = e
 	e.prev.next = e
 	e.next.prev = e
+	e.list = l
 	l.len++
 	return e
 }
+
+// insert value
+func (l *List) insertValue(v interface{}, at *Element) *Element {
+	return l.insert(&Element{
+		Value: v,
+	}, at)
+}
+
+// remove a value
+// why = nil can avoid memory leaks
+func (l *List) remove(e *Element) *Element {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	e.next = nil
+	e.prev = nil
+	e.list = nil
+	l.len--
+	return e
+}
+
+// 
+
