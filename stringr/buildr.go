33b7eba82ae48ce94cf9a1f859a9b3c4e852a513
lbs <943788110@qq.com>
2020-05-07 10:47:56 +0800

    add some methods to stringr

--- stringr/buildr.go
+++ stringr/buildr.go
@@ -7,8 +7,41 @@
 
 package stringr
 
+import "unsafe"
+
 type Buildr struct {
 	addr *Buildr
 	buf []byte
 }
 
+func (b *Buildr) Grow(n int) {
+	b.copyCheck()
+	if n < 0 {
+		panic("stringr.Buildr.Grow: negative param")
+	}
+	if cap(b.buf) - len(b.buf) < n {
+		b.grow(n)
+	}
+}
+
+func (b *Buildr) grow(n int) {
+	buf := make([]byte, len(b.buf), 2 * cap(b.buf) + n)
+	copy(buf, b.buf)
+	b.buf = buf
+}
+
+func (b *Buildr) copyCheck() {
+	if b.addr == nil {
+		b.addr = (*Buildr)(noescape(unsafe.Pointer(b)))
+	}
+}
+
+func noescape(p unsafe.Pointer) unsafe.Pointer {
+	x := uintptr(p)
+	return unsafe.Pointer(x ^ 0)
+}
+
+func (b *Buildr) Reset() {
+	b.addr = nil
+	b.buf = nil
+}
