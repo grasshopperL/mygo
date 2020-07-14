c4961d6a407a54849158c1992a935cff7d21822f
lbs <943788110@qq.com>
2020-07-14 19:01:41 +0800

    add ringr and some methods

---
+++ container/ringr.go
@@ -0,0 +1,69 @@
+/**
+ * @Author: liubaoshuai3
+ * @Date: 2020/7/14 18:14
+ * @File: ringr
+ * @Description:
+ */
+
+package container
+
+type Ring struct {
+	prev, next *Ring
+	Value interface{}
+}
+
+func (r *Ring) init() *Ring {
+	r.next = r
+	r.prev = r
+	return r
+}
+
+func (r *Ring) Next() *Ring {
+	if r.next == nil {
+		return r.init()
+	}
+	return r.next
+}
+
+func (r *Ring) Prev() *Ring {
+	if r.next == nil {
+		return r.init()
+	}
+	return r.prev
+}
+
+func (r *Ring) Move(n int) *Ring {
+	if r.next == nil {
+		return r.init()
+	}
+	switch {
+	case n < 0:
+		for ; n < 0; n++ {
+			r = r.prev
+		}
+	case n > 0:
+		for ; n > 0; n-- {
+			r = r.next
+		}
+	}
+	return r
+}
+
+func New(n int) *Ring {
+	if n <= 0 {
+		return nil
+	}
+	r := new(Ring)
+	p := r
+	for i := 1; i < n; i++ {
+		p.next = &Ring{
+			prev:  p,
+			next:  nil,
+			Value: nil,
+		}
+		p = p.next
+	}
+	p.next = r
+	r.prev = p
+	return r
+}
