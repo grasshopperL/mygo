c50d185d7330728c2b2e4df1fd532faff302c22f
lbs <943788110@qq.com>
2020-08-25 20:22:01 +0800

    add buffer

--- bytesr/buffers.go
+++ bytesr/buffers.go
@@ -7,6 +7,8 @@
 
 package bytesr
 
+import "errors"
+
 const smallBufferSize = 64
 
 type Buffer struct {
@@ -25,4 +27,43 @@ const (
 	opReadRune3 readOp = 3  // Read rune of size 3.
 	opReadRune4 readOp = 4  // Read rune of size 4.
 )
+
+var ErrTooLarge = errors.New("bytesr.buffers: too large")
+var errNegativeRead = errors.New("bytesr.buffers: reader returned negative count from read")
+const maxInt = int(^uint(0) >> 1)
+
+func (b *Buffer) Bytes() []byte {
+	return b.buf[b.off:]
+}
+
+func (b *Buffer) String() string {
+	if b.buf == nil {
+		return "<nil>"
+	}
+	return string(b.buf[b.off:])
+}
+
+func (b *Buffer) empty() bool {
+	return len(b.buf) <= b.off
+}
+
+func (b *Buffer) Len() int {
+	return len(b.buf) - b.off
+}
+
+func (b *Buffer) Cap() int {
+	return cap(b.buf)
+}
+
+func (b *Buffer) Truncate(n int) {
+	if n == 0 {
+		b.Reset()
+		return
+	}
+	b.lastRead = opInvalid
+	if n < 0 || n > b.Len() {
+		panic("bytesr.buffer: truncation out of range")
+	}
+	b.buf = b.buf[:b.off+n]
+}
 
